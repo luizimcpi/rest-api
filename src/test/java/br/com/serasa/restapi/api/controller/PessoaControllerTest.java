910cc6665c331bd20037197ad93c9b3dc0eb2540
Luiz Evangelista <luizhenrique.se@gmail.com>
2022-04-19 14:26:38 -0300

    fix: ajustando testes de controller após introdução do spring security

--- src/test/java/br/com/serasa/restapi/api/controller/PessoaControllerTest.java
+++ src/test/java/br/com/serasa/restapi/api/controller/PessoaControllerTest.java
@@ -7,19 +7,22 @@ import br.com.serasa.restapi.exception.NoContentException;
 import br.com.serasa.restapi.persistence.entity.Pessoa;
 import br.com.serasa.restapi.service.PessoaService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
@@ -32,22 +35,34 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest( controllers = PessoaController.class)
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PessoaControllerTest {
 
     private static final String URL = "/pessoa";
-    private String token = "";
 
     @Autowired
-    MockMvc mvc;
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
 
     @MockBean
     PessoaService pessoaService;
 
+    @BeforeAll
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+
+    @WithMockUser("spring")
     @Test
     public void deveCriarUmaPessoaComSucesso() throws Exception {
         PessoaRequest requestDto = REQUEST_PESSOA_VALIDA;
@@ -67,6 +82,7 @@ public class PessoaControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isCreated());
     }
 
+    @WithMockUser("spring")
     @Test
     public void deveRetornarBadRequestQuandoParametroNomeNaoEstiverPreenchido() throws Exception {
         PessoaRequest requestDto = REQUEST_PESSOA_SEM_NOME_PREENCHIDO;
@@ -82,6 +98,7 @@ public class PessoaControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
 
+    @WithMockUser("spring")
     @Test
     public void deveRetornarBadRequestQuandoParametroScoreForMaiorQueMil() throws Exception {
         PessoaRequest requestDto = REQUEST_PESSOA_SCORE_MAIOR_QUE_MIL;
@@ -97,6 +114,7 @@ public class PessoaControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
 
+    @WithMockUser("spring")
     @Test
     public void deveRetornarBadRequestQuandoParametroScoreForMenorQueZero() throws Exception {
         PessoaRequest requestDto = REQUEST_PESSOA_SCORE_MENOR_QUE_MIL;
@@ -112,6 +130,7 @@ public class PessoaControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
 
+    @WithMockUser("spring")
     @Test
     public void deveRetornarUmaPessoaPorIdComSucesso() throws Exception {
 
@@ -140,6 +159,7 @@ public class PessoaControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.estados", hasSize(4)));
     }
 
+    @WithMockUser("spring")
     @Test
     public void deveRetornarErroQuandoBuscarUmaPessoaPorIdComIdInvalido() throws Exception {
 
@@ -153,6 +173,7 @@ public class PessoaControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isNoContent());
     }
 
+    @WithMockUser("spring")
     @Test
     public void deveRetornarPessoasComSucesso() throws Exception {
 
@@ -176,6 +197,7 @@ public class PessoaControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
+    @WithMockUser("spring")
     @Test
     public void deveRetornarNoContentQuandoNaoEncontrarPessoas() throws Exception {
 
